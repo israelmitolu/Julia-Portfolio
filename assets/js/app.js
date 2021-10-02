80ef987672936a68aca0ed2fc11aec0e697dd188
IsraelMitolu <israelmitolu@gmail.com>
2021-10-02 22:24:10 +0100

    .

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+/*==================== PRELOADER ====================*/
+$(function() {
+    $(window).on("load", function() {
+      $('.preloader').fadeOut('slow', function() {
+        $(this).remove();
+      });
+    });
+  });
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.hero-intro h1, .hero-intro p, .hero-img, .social-links a, .experience-col, .contact-col`, {
+    interval: 200
+})
