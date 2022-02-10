010248eac33e97d9c5c09521f7ad261c3e47b50e
Israel Mitolu <israelmitolu@gmail.com>
2022-02-10 19:52:20 +0100

    organized starter files

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -23,35 +23,3 @@ sr.reveal(
 );
 
 /*==================== CUSTOM CURSOR ====================*/
-let cursorBall = document.querySelector(".cursor-ball");
-let cursorOutline = document.querySelector(".cursor-outline");
-let links = document.querySelectorAll(".social-links a");
-
-document.addEventListener("mousemove", (e) => {
-  cursorBall.style.top = e.pageY + "px";
-  cursorBall.style.left = e.pageX + "px";
-
-  cursorOutline.style.top = e.pageY + "px";
-  cursorOutline.style.left = e.pageX + "px";
-});
-
-document.addEventListener("mousedown", (e) => {
-  if (e.button === 0) {
-    cursorOutline.classList.add("cursor-mousedown");
-  }
-});
-document.addEventListener("mouseup", () => {
-  cursorOutline.classList.remove("cursor-mousedown");
-});
-
-links.forEach((link) => {
-  link.addEventListener("mouseover", () => {
-    cursorOutline.classList.add("scale-link");
-    link.classList.add("hovered-link");
-  });
-
-  link.addEventListener("mouseleave", () => {
-    cursorOutline.classList.remove("scale-link");
-    link.classList.remove("hovered-link");
-  });
-});
